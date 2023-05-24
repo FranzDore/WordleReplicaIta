acc6d1d573adfa81716d3447e7075ae77d1d4c1a
Francesco Dore <82588605+FranzDore@users.noreply.github.com>
2023-05-24 17:54:29 +0200

    Cleaned code

--- src/Server_Task.java
+++ src/Server_Task.java
@@ -16,10 +16,7 @@ import java.util.List;
 import com.google.gson.*;
 import com.google.gson.stream.JsonReader;
 
-/*
- * TODO: Finire Multicast:
- * 		 L'Handler dei messaggi multicast non riceve niente, rimane in receive.
- * 		 Prova dal GitHub di Gioele www.github.com/pavomod
+/*		 
  * 		 
  */
 
@@ -27,26 +24,27 @@ import com.google.gson.stream.JsonReader;
 public class Server_Task implements Runnable{
 	public Socket socket;
 	public InetAddress multicastAddr;
+	public int ms_port;
 	
-	
-	public Server_Task(Socket socket, InetAddress multicastAddr) {
+	public Server_Task(Socket socket, InetAddress multicastAddr, int ms_port) {
 		this.socket = socket;
 		this.multicastAddr = multicastAddr;
+		this.ms_port = ms_port;
 	}
 	
 	public void run() {
 		try(DataInputStream reader = new DataInputStream(this.socket.getInputStream());
 			DataOutputStream writer = new DataOutputStream(this.socket.getOutputStream());){
 			
-			boolean closed = false;
+			boolean closed = false; //if true, client was closed.
 			while(! closed) {
 				int choice = reader.readInt();
 				switch(choice) {
 				case 1:
-					handleRegistration(this.socket, reader, writer, this.multicastAddr);
+					handleRegistration(this.socket, reader, writer, this.multicastAddr, this.ms_port);
 					break;
 				case 2:
-					handleLogin(this.socket, reader, writer, this.multicastAddr);
+					handleLogin(this.socket, reader, writer, this.multicastAddr, this.ms_port);
 					break;
 				case 3:
 					System.out.println("Connection was closed by Client > " + this.socket.getInetAddress() + "\n");
@@ -64,24 +62,24 @@ public class Server_Task implements Runnable{
 	}
 	
 	//Passo anche la socket per prendere l'address (.getInetAddress().toString())
-	private static void handleRegistration(Socket socket, DataInputStream in, DataOutputStream out, InetAddress ia) {
+	private static void handleRegistration(Socket socket, DataInputStream in, DataOutputStream out, InetAddress ia, int ms_port) {
 		boolean work = true;
-		try(JsonReader json_in = new JsonReader(new FileReader("accounts.json"));){
+		try(JsonReader json_in = new JsonReader(new FileReader("../bin/accounts.json"));){
 			
 			while(work) {
-				String username_password = in.readUTF(); //Riceviamo username e password in una botta, separati da virgola.
+				String username_password = in.readUTF(); //Receive username and passward together, separated by a comma.
 				String [] arr = username_password.split(",");
-				if(arr.length != 2) { //Qualcosa è andato storto nell'invio di username o password.
+				if(arr.length != 2) { //Something went wrong when reading either username or password.
 					out.writeBoolean(false);
 					continue;
 				}
 				
 				String username = arr[0];
-				boolean isUsed = isUsed_Username(username);
+				boolean isUsed = isUsed_Username(username); //Check if username is already used. 
 				
-				String password = arr[1].trim(); //cancelliamo i white-space davanti e dopo la password.
+				String password = arr[1].trim(); //Delete all leading and trailing whitespaces.
 				boolean pw_check;
-				//Controlliamo che la password non sia la stringa vuota e non abbia spazi bianchi all'interno
+				//Check that password isn't empty string and that it doesn't contain whitespaces in the middle.
 				if(password.equals("") || password.contains(" ")) {
 					pw_check = false;
 					continue;
@@ -89,7 +87,7 @@ public class Server_Task implements Runnable{
 					pw_check = true;
 				}
 				
-				boolean overall = !isUsed && pw_check;
+				boolean overall = !isUsed && pw_check; //If conditions are met, it will be true and we can move on.
 				out.writeBoolean(overall);
 				if(! overall)	continue;
 				
@@ -100,15 +98,15 @@ public class Server_Task implements Runnable{
 			}
 			System.out.println("Registration was successfull > Client " + socket.getInetAddress().toString() + "\n");
 			
-			handleLogin(socket, in, out, ia); //Right after registering, user is asked to login.
+			handleLogin(socket, in, out, ia, ms_port); //Right after registering, user is asked to login.
 			
 		} catch(IOException e) {
-			e.printStackTrace();
+			;
 		}
 	}
 	
 	//Passo anche la socket per prendere l'address (.getInetAddress().toString())
-	private static void handleLogin(Socket socket, DataInputStream in, DataOutputStream out, InetAddress ia) {
+	private static void handleLogin(Socket socket, DataInputStream in, DataOutputStream out, InetAddress ia, int ms_port) {
 		try {
 			Gson gson = new GsonBuilder().setPrettyPrinting().create();
 			
@@ -116,14 +114,12 @@ public class Server_Task implements Runnable{
 			Account temp = null;
 			boolean work = true;
 			
-			//Chiediamo username e password al Client
+			//Ask for username and password to client.
 			while(work) {
 				
-				JsonReader json_in = new JsonReader(new FileReader("accounts.json"));				String username = in.readUTF().trim();
+				JsonReader json_in = new JsonReader(new FileReader("../bin/accounts.json"));				String username = in.readUTF().trim();
 				String password = in.readUTF().trim();
-				
-				//System.out.println("US: " + username + " PW: " + password + "\n");
-								//Carichiamo la lista di tutti gli account
+								//Load list of all accounts
 				List<Account> list = new ArrayList<Account>();
 				json_in.beginArray();
 				while (json_in.hasNext()) {
@@ -132,19 +128,19 @@ public class Server_Task implements Runnable{
 				}
 				json_in.endArray();	
 				boolean doesExist = false;
-				temp = lookForName(list, username);		//Troviamo Account usando lo username fornito. (Metodo statico)
+				temp = lookForName(list, username);		//Find account using given username (Static method).
 				if(temp != null)
 					doesExist = true;
 				
-				if(doesExist == false) {				//Controlliamo che l'account richiesto esista.
+				if(doesExist == false) {				//Check if account exists.
 					out.writeBoolean(false);
 					json_in.close();
 					continue;
 				} else {
-					out.writeBoolean(true);  			//Bisogna mettere un if-then-else, altrimenti non possiamo mettere il continue per tornare su.
+					out.writeBoolean(true);  			//if-then-else is needed to use "continue" in case something goes wrong.
 				}
 				
-				if(! password.equals(temp.password)) {	//Controlliamo se le password coincidono.
+				if(! password.equals(temp.password)) {	//Check if passwords are the same.
 					out.writeBoolean(false);
 					json_in.close();
 					continue;
@@ -157,7 +153,7 @@ public class Server_Task implements Runnable{
 			}
 			
 			Checker checker = new Checker();
-			//Impediamo il login se l'account risulta essere già loggato. (Si veda la classe "Checker.java")
+			//We prevent login if account is already logged in (Look at "Checker.java" class)
 			if(checker.isAlreadyLoggedIn(temp)) {
 				out.writeBoolean(true);
 				return;
@@ -166,21 +162,18 @@ public class Server_Task implements Runnable{
 			
 			System.out.println("\n > Client " + socket.getInetAddress().toString() + " logged in.\n");
 			
-	
-			
-						
-			//Ora può iniziare la fase di gioco
-			checker.place(temp); //aggiungiamo l'account alla lista di quelli attivi
-			gameHandler(socket, in, out, temp, ia);
+			//Game Phase is now accessible.
+			checker.place(temp); //add the account to list of active users
+			gameHandler(socket, in, out, temp, ia, ms_port);
 			checker.delete(temp); //if gameHandler method has returned, it means account has logged out: we remove him from acctive players.
 			
 			
 		} catch(IOException e) {
-			e.printStackTrace();
+			;
 		}
 	}
 	
-	private static void gameHandler(Socket socket,DataInputStream in,DataOutputStream out,Account account,InetAddress ia) {
+	private static void gameHandler(Socket socket,DataInputStream in,DataOutputStream out,Account account,InetAddress ia, int ms_port) {
 		try {
 			boolean work = true;
 			while(work) {
@@ -193,7 +186,7 @@ public class Server_Task implements Runnable{
 					sendStats(in, out, account);
 					break;
 				case 3:
-					handleShareRequest(in, out, account, ia);
+					handleShareRequest(in, out, account, ia, ms_port);
 					break;
 				case 4:
 					/* Il server non riceverà mai un codice di tipo 4.
@@ -201,7 +194,7 @@ public class Server_Task implements Runnable{
 					 * coinvolge in alcun modo il server.*/
 					break;
 				case 5:
-					//Salviamo i dati relativi alla sessione prima di fare il logout.
+					//We save data relative to last session before logging out.
 					saveSession(account);
 					return; //We return to handleLogin method and socket gets closed there. No resource leak.
 				default:
@@ -221,19 +214,19 @@ public class Server_Task implements Runnable{
 			String parolaCorretta = WordSelector.getWord();
 			
 			if(! account.lastWord.equals("null")) {
-				if(account.lastWord.equals(parolaCorretta)) { //Controllo che l'ultima parola giocata sia diversa dalla daily word.
+				if(account.lastWord.equals(parolaCorretta)) { //Check if last guessed word is  the same daily word.
 					System.out.println("Account " + account.username + " has played already.\n");
-					out.writeBoolean(true); //true = ha già giocato.
+					out.writeBoolean(true); //true = has already played.
 					return;
 				}
 			}
 			
-			//Se non ha già giocato, non entro nell'if, quindi non eseguo la return e posso scrivere false nella socket.
+			//If they haven't played they don't go into the "if" statement, they don't return and can write false into the socket.
 			out.writeBoolean(false);
 			
 			
-			//Inizializziamo l'account a seguito della richiesta di inizio nuova partita.
-			account.lastWord = parolaCorretta; //impediamo che possa rigiocare finché non viene scelta una nuova parola.
+			//Change account information right before starting the game
+			account.lastWord = parolaCorretta; //By changing this parameter, we deny them from playing again until daily word changes.
 			account.wasSolved = false;
 			account.playedGames++;
 			
@@ -241,12 +234,12 @@ public class Server_Task implements Runnable{
 			while(turno <= 12) {
 				String userGuess = in.readUTF().trim();
 				
-				boolean exists = binarySearch(file, userGuess); //Controlliamo se esista la parola nel file di parole.
+				boolean exists = binarySearch(file, userGuess); //Check if word exists in the words file.
 				out.writeBoolean(exists);
 				
 				if(! exists)	continue;
 				
-				HashMap<Character, Integer> map = new HashMap<Character, Integer>(); //Servirà per contare le occorrenze nella parola segreta
+				HashMap<Character, Integer> map = new HashMap<Character, Integer>(); //Needed to count occurrences of the word.
 				for(int i=0; i<parolaCorretta.length(); i++) {
 					char lettera = parolaCorretta.charAt(i);
 					if(map.containsKey(lettera))
@@ -254,12 +247,12 @@ public class Server_Task implements Runnable{
 					else map.put(lettera, 1);
 				}
 				
-				//Controlliamo la parola fornita, e inseriamo i suggerimenti in "res".
+				//Check given word and insert tips into "res" variable.
 				String res = null;
 				res = checkGreenLetters(userGuess, parolaCorretta, map);
 				res = checkYellowLetters(userGuess, parolaCorretta, map, res);
 				
-				//Mandiamo la stringa ottenuta dopo i controlli. "Se composta solo da '+' allora chiudiamo la partita (vittoria).
+				//Send string after the adequate checks. If res is only made of '+' then we end game (Victory).
 				out.writeUTF(res);
 				out.flush();
 				
@@ -268,7 +261,7 @@ public class Server_Task implements Runnable{
 					account.guessDistribution[turno - 1]++;
 					account.lastWordGuesses = turno;
 					account.wasSolved = true;
-					return; //Usciamo da "PlayWordle".
+					return; //Exit "PlayWordle" since they won.
 				}
 				turno++;	
 			}
@@ -279,6 +272,7 @@ public class Server_Task implements Runnable{
 		}
 	}
 
+	//Simply builds the string to be shown when Client asks for stats, and sends it through the socket.
 	private static void sendStats(DataInputStream in, DataOutputStream out, Account account) {
 		try {
 			out.writeUTF(account.username);
@@ -300,20 +294,21 @@ public class Server_Task implements Runnable{
 		}
 	}
 	
-	private static void handleShareRequest(DataInputStream in, DataOutputStream out, Account account, InetAddress ia) {
+	//Just like above, builds the string and sends it through the socket.
+	private static void handleShareRequest(DataInputStream in, DataOutputStream out, Account account, InetAddress ia, int ms_port) {
 		try {
-			if(!account.lastWord.equals(WordSelector.getWord())) {
+			if(!account.lastWord.equals(WordSelector.getWord())) { //If they haven't played, nothing can be shared. We return false.
 				out.writeBoolean(false);
 				return;
 			}
-			out.writeBoolean(true);
+			out.writeBoolean(true); //We share the stats of last game.
 			
 			String toBeShared;
 			if(account.wasSolved)
 				toBeShared = "'"+account.username+"' ha indovinato la parola in "+account.lastWordGuesses+" tentativi!";
 			else toBeShared = "'" + account.username + "' non ha indovinato la parola";
 			
-			DatagramPacket msgAsPacket = new DatagramPacket(toBeShared.getBytes(), toBeShared.length(), ia, 3456);
+			DatagramPacket msgAsPacket = new DatagramPacket(toBeShared.getBytes(), toBeShared.length(), ia, ms_port);
 			MulticastSocket ms_socket = new MulticastSocket();
 			ms_socket.send(msgAsPacket);
 			ms_socket.close();
@@ -326,6 +321,7 @@ public class Server_Task implements Runnable{
 		
 	}
 	
+	//Binary search to look through the words.
 	public static boolean binarySearch(RandomAccessFile f, String key) throws IOException {
         final int numElements = ((int) f.length() / 11);
 
@@ -341,14 +337,16 @@ public class Server_Task implements Runnable{
         return false;
     }
 	
+	//returns true if username is used, false otherwise
 	private static boolean isUsed_Username(String username) {
 
 		boolean res = false;
 		
-		try(JsonReader reader = new JsonReader(new FileReader("accounts.json"));){
+		try(JsonReader reader = new JsonReader(new FileReader("../bin/accounts.json"));){
 			Gson gson = new Gson();
 			List<Account> users = new ArrayList<Account>();
 			
+			//Load all users
 			reader.beginArray();
 			while (reader.hasNext()) {
 				Account account = gson.fromJson(reader, Account.class);
@@ -356,6 +354,7 @@ public class Server_Task implements Runnable{
 			}
 			reader.endArray();
 			
+			//Look through the list, if we find a match we return true.
 			for(Account x : users) {
 				if(username.equals(x.username)) {
 					res = true;
@@ -368,7 +367,8 @@ public class Server_Task implements Runnable{
 		}
 		return res;
 	}
-
+	
+	//returns the actual account, not a boolean value. See isUsed_Username function for that matter
 	private static Account lookForName(List<Account> lista, String username) {
 		for(Account x : lista) {
 			if(username.equals(x.username)) {
@@ -378,12 +378,13 @@ public class Server_Task implements Runnable{
 		return null;
 	}
 
+	//Saves session. All data will be saved afer calling this function. SYNCHRONIZED since we access a shared structure (.json file)
 	private static synchronized void saveSession(Account account) {
-		try(JsonReader json_in = new JsonReader(new FileReader("accounts.json"));){
+		try(JsonReader json_in = new JsonReader(new FileReader("../bin/accounts.json"));){
 			
 			Gson gson = new GsonBuilder().setPrettyPrinting().create();
 			
-			//Carichiamo la lista degli utenti dal file Json.
+			//Load list of all users.
 			List<Account> users = new ArrayList<Account>();
 			json_in.beginArray();
 			while (json_in.hasNext()) {
@@ -392,6 +393,7 @@ public class Server_Task implements Runnable{
 			}
 			json_in.endArray();
 			
+			//We remove the old instance of the account...
 			for(Account x : users) {
 				if(x.username.equals(account.username)) {
 					users.remove(x);
@@ -399,12 +401,12 @@ public class Server_Task implements Runnable{
 				}	
 			}
 			
-			//aggiungiamo l'account "modificato" alla lista
+			//...so that we can add the modified and new version of it
 			users.add(account);
 			
-			//Ora abbiamo solo bisogno di riscrivere nel file tutti gli account nella lista, che ora comprende anche l'utente modificato
+			//Only need to rewrite the list into the file. The list now containes the new account, and so will the file after writing.
 			String listAsString = gson.toJson(users);
-			Writer writer = new FileWriter("accounts.json");
+			Writer writer = new FileWriter("../bin/accounts.json");
 			writer.write(listAsString);
 			writer.close();
 			
@@ -413,9 +415,10 @@ public class Server_Task implements Runnable{
 		}
 	}
 	
+	//Same as before, but we dont need to loop through the list, we just add the account to it and re-write.
 	private static synchronized void saveAccount(String username, String password, JsonReader json_in) {
 		try {
-			Account new_acc = new Account(username, password); //Nuova istanza della classe Account.
+			Account new_acc = new Account(username, password); //New instance of class Account.
 			List<Account> users = new ArrayList<Account>();
 			
 			Gson gson = new GsonBuilder().setPrettyPrinting().create();
@@ -427,12 +430,12 @@ public class Server_Task implements Runnable{
 			}
 			json_in.endArray();
 			
-			//Dopo aver caricato la lista degli utenti dal file, aggiungiamo la nuova istanza alla lista.
+			//We add new account to the list of already existing accounts.
 			users.add(new_acc);
 			
-			//Ora abbiamo solo bisogno di riscrivere nel file tutti gli account nella lista, che ora comprende anche la nuova istanza
+			//Only need to rewrite the list into the file. The list now containes the new account, and so will the file after writing.
 			String listAsString = gson.toJson(users);
-			Writer writer = new FileWriter("accounts.json");
+			Writer writer = new FileWriter("../bin/accounts.json");
 			writer.write(listAsString);
 			writer.close();
 			
@@ -441,31 +444,33 @@ public class Server_Task implements Runnable{
 		}
 		
 	}
-
+	
+	//Function checks for green letters and updates string and HashMap
 	private static String checkGreenLetters(String userGuess, String parolaCorretta, HashMap<Character, Integer> map) {
 		String result = "XXXXXXXXXX";
 		
-		for(int i=0; i<userGuess.length(); i++) {
-			char usr = userGuess.charAt(i);
+		for(int i=0; i<userGuess.length(); i++) { 	//we iterate over the user's guess
+			char usr = userGuess.charAt(i);			
 			char wrd = parolaCorretta.charAt(i);
-			if(usr == wrd) {
-				result = result.substring(0, i) + '+' + result.substring(i + 1);
+			if(usr == wrd) {						//check if the letters of the user's guess and hidden word at "i" position are the same.
+				result = result.substring(0, i) + '+' + result.substring(i + 1);	//We add a '+' in the result at corresponding position.
 				map.put(wrd, map.get(wrd) - 1); //decrementiamo il valore associato alla lettera.
 			}
 		}
 			
 		return result;
 	}
-
+	
+	//Function checks for yellow letters and updates string and HashMap
 	private static String checkYellowLetters(String userGuess, String parolaCorretta, HashMap<Character, Integer> map, String result) {
 		for(int i=0; i<userGuess.length(); i++) {
 			char usr = userGuess.charAt(i);
 			
-			if(result.charAt(i) != '+')
-				if(map.containsKey(usr))
-					if(map.get(usr) > 0) {
-						map.put(usr, map.get(usr) - 1);
-						result = result.substring(0, i) + '?' + result.substring(i + 1);
+			if(result.charAt(i) != '+')		//If it is '+' it means we already counted before in the "checkGreenLetters" method.
+				if(map.containsKey(usr))	//Is the letter in the HashMap? (Is the character in the hidden word?)
+					if(map.get(usr) > 0) {	
+						map.put(usr, map.get(usr) - 1); //decrement value
+						result = result.substring(0, i) + '?' + result.substring(i + 1); //Add a '?': letter exists in a different spot.
 					}
 		}
 		return result;
